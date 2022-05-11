80272f308ab8df6ef387a1e66080232c6c59c45e
Salvador Alvarez <sao.95@hotmail.com>
2022-05-10 18:38:46 -0700

    se agrego la libreria axios para consumir las apis

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?api_key=a436c
 const API_URL_FAVOTITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=a436c5db-c690-4c4b-a598-fee16aba6889`;
 const API_URL_UPLOAD = 'https://api.thecatapi.com/v1/images/upload';
 
+
+//agregar Axios
+const api = axios.create({
+    baseURL: 'https://api.thecatapi.com/v1'
+  });
+  api.defaults.headers.common['X-API-KEY'] = 'c08d415f-dea7-4a38-bb28-7b2188202e46';
+
+  // solo se agre la base de la url y el head ya lleva impiclita la api key
+
 const spanError = document.getElementById('error')
 
 async function loadRandomMichis() {
@@ -66,29 +75,38 @@ async function loadFavouriteMichis() {
 }
 
 async function saveFavouriteMichi(id) {
-  const res = await fetch(API_URL_FAVOTITES, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      image_id: id
-    }),
+// guarda michis a travez de AXIOS
+const { data, status } = await api.post('/favourites', {
+    image_id: id,
   });
-  const data = await res.json();
+
+
+
+//metod con FETCH
+//   const res = await fetch(API_URL_FAVOTITES, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       image_id: id
+//     }),
+//   });
+//   const data = await res.json();
 
   console.log('Save')
-  console.log(res)
-
-  if (res.status !== 200) {
-    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
-  }
-  else {
-      //una vez presionado el boto guardar favorito si todo sale bien ejecutar la funcion loadFavouriteMichis la cual recarga todos los elementos guardados
+  if (status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + status + data.message;
+  } else {
+       //una vez presionado el boto guardar favorito si todo sale bien ejecutar la funcion loadFavouriteMichis la cual recarga todos los elementos guardados
     console.log('Michi guardado en favoritos')
     loadFavouriteMichis();
+  }
 }
-}
+
+
+
+
 async function deleteFavouriteMichi(id) {
     const res = await fetch(API_URL_FAVOTITES_DELETE(id), {
       method: 'DELETE',
@@ -132,6 +150,30 @@ async function deleteFavouriteMichi(id) {
     }
   }
   
+  function miniatura() {
+    const form = document.getElementById('uploadingForm')
+    const formData = new FormData(form)
+	//usamos el FileReader para sacar la información del archivo del formData
+    const reader = new FileReader();
+
+//Este código es para borrar la miniatura anterior al actualizar el form.
+    if (form.children.length === 3) {
+        const preview = document.getElementById("preview")
+        form.removeChild(preview)
+    }
+//aquí sucede la magia, el reader lee los datos del form.
+    reader.readAsDataURL(formData.get('file'))
+
+//Éste código es para cuando termine de leer la info de la form, cree una imagen miniatura de lo que leyó el form.
+    reader.onload = () => {
+        const previewImage = document.createElement('img')
+        previewImage.id = "preview"
+        previewImage.width = 50
+        previewImage.src = reader.result
+        form.appendChild(previewImage);
+    }
+
+}
 
 loadRandomMichis();
 loadFavouriteMichis();
